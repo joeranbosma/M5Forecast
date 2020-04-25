248f22d258302923be9344766175c8805020ef8a
Joeran <joeran@bosma.co>
2020-04-25 17:08:58 +0200

    Wrote BatchCreator to select source & target days for DNN training

---
+++ train.py
@@ -0,0 +1,131 @@
+"""
+M5Forecast - Training
+# Implementation of M5 Forecasting challenge on Kaggle, https://www.kaggle.com/c/m5-forecasting-accuracy/.
+
+Created: 25 apr 2020
+"""
+
+import numpy as np
+
+from flow import select_day_nums
+
+
+class BatchCreator(object):
+    """Batch creator for M5Forecast - Accuracy challenge
+    Expects a DataFrame with the days as index (d_num_start, .., d_num_end)
+
+    - features: which columns to use for making predictions.
+    - labels: which columns to predict.
+    - window_in: number of input days for making predictions.
+    - window_out: number of days to predict.
+    - dilation: step size for training day selection.
+    - lag: number of days between window_in days and window_out days.
+    - shuffle: whether to shuffle the samples, where a sample consists of
+               both the window_in days, window_out days.
+    """
+
+    def __init__(self, df, features, labels, window_in, window_out, dilation=1,
+                 lag=0, batch_size=32, shuffle=True, ensure_all_samples=False):
+        """Initialization"""
+        # Save a reference to the df
+        self.df = df
+        self.features = features
+        self.labels = labels
+
+        # Save hyperparameters
+        self.window_in = window_in
+        self.window_out = window_out
+        self.dilation = dilation
+        self.lag = lag
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        self.ensure_all_samples = ensure_all_samples
+
+        # Set up list of start indices for the validation set
+        # From those the other indices will be calculated
+        # Have 1 + (window_in - 1) * dilation training samples
+        # Need `lag` days between training and validation samples
+        train_day_span = 1 + (window_in - 1) * dilation
+        start_val_day_min = min(select_day_nums(df, axis=0)) + train_day_span + lag
+        start_val_day_max = max(select_day_nums(df, axis=0)) - window_out + 1
+        self.list_start_val_days = np.arange(start_val_day_min, start_val_day_max + 1)
+
+        # initialize indices
+        self.indexes = None
+        self.on_epoch_end()
+
+        # calculate properties
+        self.n = len(self.list_start_val_days)
+        self.n_features = len(features) * window_in
+        self.n_labels = len(labels) * window_out
+
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        if self.ensure_all_samples:
+            return int(np.ceil(self.n / self.batch_size))
+        return self.n // self.batch_size
+
+    def __getitem__(self, index):
+        """Generate one batch of data"""
+
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        list_start_val_days_temp = self.list_start_val_days[indexes]
+
+        # Generate data
+        x_batch, y_batch = self.__data_generation(list_start_val_days_temp)
+
+        return x_batch, y_batch
+
+    def on_epoch_end(self):
+        """Updates indexes after each epoch"""
+        self.indexes = np.arange(len(self.list_start_val_days))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_start_val_days_temp):
+        """Generates data containing batch_size samples"""
+
+        # create batch placeholders
+        batch_size = len(list_start_val_days_temp)
+        x_batch = np.zeros(shape=(batch_size, self.n_features,))
+        y_batch = np.zeros(shape=(batch_size, self.n_labels))
+
+        # fill batch
+        for i, start_val_day in enumerate(list_start_val_days_temp):
+            """
+            start_val_day contains the first day of the evaluation set
+            - final val day: start_val_day + window_out - 1
+            - final input day: start_val_day - lag - 1
+            - start input day: final input day - (window_in - 1) * dilation
+            """
+            # calculate day nums
+            final_val_day = start_val_day + self.window_out - 1
+            final_inp_day = start_val_day - self.lag - 1
+            start_inp_day = final_inp_day - (self.window_in - 1) * self.dilation
+
+            # print("Selecting train: {}-{} (step: {})".format(start_inp_day, final_inp_day, self.dilation))
+            # print("Selecting eval: {}-{} (lag: {})".format(start_val_day, final_val_day, self.lag))
+
+            # create lists of indices
+            val_idx = ['d_%d' % d for d in range(start_val_day, final_val_day + 1)]
+            inp_idx = ['d_%d' % d for d in range(start_inp_day, final_inp_day + 1, self.dilation)]
+
+            # print("Train idx: {}".format(inp_idx))
+
+            x_batch[i] = self.df.loc[inp_idx, self.features].values.T.reshape(self.n_features)
+            y_batch[i] = self.df.loc[val_idx, self.labels].values.T.reshape(self.n_labels)
+
+        return x_batch, y_batch
+
+    def flow(self):
+        """returns a generator that will yield batches infinitely"""
+        while True:
+            for index in range(self.__len__()):
+                batch_x, batch_y = self.__getitem__(index)
+                yield batch_x, batch_y
+            self.on_epoch_end()
+
+
