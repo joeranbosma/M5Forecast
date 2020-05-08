617f2fee0d9f6bc05f3f2137902c8e44cecdd4d3
a-t-0 <eai1@protonmail.com>
2020-05-08 17:16:01 +0200

    Further developed model.

--- custom_layer/CustomLayerNN.py
+++ custom_layer/CustomLayerNN.py
@@ -8,37 +8,77 @@
 # 2. loads a test set
 # 3. Uses the trained model to perform a prediction/classification
 
+# basic imports
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm as tqdm
+import matplotlib.pyplot as plt
 
-# 0. loads a training dataset 
-def get_train_data():
-	pass
+# tensorflow imports
+from tensorflow.keras.layers import Input, Dense, Conv1D, MaxPooling1D, Reshape, Flatten
+# from tensorflow.keras.models import Model
+# from tensorflow.keras import backend as K
 
-# 1. trains a neural network (model) on the dataset
+class custom_layer_nn:
 
+	def __init__(self):
+		print("Hello world")
+		pass
 
-# Code:
-def get_model(inp_shape, quantiles):
-    # clear previous sessions
-    K.clear_session()
-
-    inp = Input(inp_shape, name="input")
-    x = inp
-    x = Dense(16)(x)
-    x = Dense(32)(x)
-    x = Dense(64)(x)
-    x = Dense(2)(x)  # represents mu, sigma
-    x = DistributionLayer(quantiles=quantiles)(x)  # returns 7 points, one for each quantile
-    out = x
-
-    model = Model(inputs=inp, outputs=out)
-
-    return model
+	# 0. loads a training dataset 
+	def get_train_data(self):
+		train_df = pd.read_csv("features.csv", index_col=0)
+		target_df = pd.read_csv("targets.csv", index_col=0)
+		
+		print('Training data shape : ', train_df.shape, target_df.shape)
+		pass
 	
+	# preprocess data
+	def preproces(self,train_df):
+		train_X = train_df.reshape(-1, 28,28, 1) # if necessesary
+		return train_df
 	
-# 2. loads a test set
-def get_test_data():
-	pass
-
-# 3. Uses the trained model to perform a prediction/classification
-def predict_output():
-	pass
+	# 1. trains a neural network (model) on the dataset
+	def train_model(model,train_df,target_df):
+		batch_size = 64
+		epochs = 20
+		num_classes = 7
+		trained_model = model.fit(train_df, target_df, batch_size=batch_size,epochs=epochs,verbose=1)
+	
+	# Code:
+	def get_model(self, inp_shape, quantiles):
+		# clear previous sessions
+		K.clear_session()
+		
+		inp = Input(inp_shape, name="input")
+		x = inp
+		x = Dense(16)(x)
+		x = Dense(32)(x)
+		x = Dense(64)(x)
+		x = Dense(2)(x)  # represents mu, sigma
+		
+		x = self.DistributionLayer(quantiles=quantiles)(x)  # returns 7 points, one for each quantile
+		out = x
+		
+		model = Model(inputs=inp, outputs=out)
+		
+		return model
+		
+		
+	# 2. loads a test set
+	def get_test_data(self):
+		pass
+	
+	# 3. Uses the trained model to perform a prediction/classification
+	def predict_output(self,train_df,target_df):
+		quantiles = [0.005, 0.025, 0.165, 0.25, 0.5, 0.75, 0.835, 0.975, 0.995]
+		pass
+	
+	# Converts smaples/weights x into the 7 x-coordinates representing the 7 quantiles
+	def DistributionLayer(self,quantiles,x):
+		pass
+	
+if __name__ == '__main__':
+	main= custom_layer_nn()
+	print("done")
