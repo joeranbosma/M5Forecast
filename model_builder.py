4484b83c2cc658838be86fc212c66b332d7f6062
Joeran <joeran@bosma.co>
2020-05-28 11:51:18 +0200

    Wrote experiment code. Some changes to the model builder

--- model_builder.py
+++ model_builder.py
@@ -46,7 +46,7 @@ def get_custom_layer(sigma_coef):
     return custom_layer
 
 
-def get_simple_dist_model(inp_shape, num_nodes=64, sigma_coefs=None, clear_session=True):
+def get_simple_dist_model(inp_shape, num_nodes=64, final_activation="linear", sigma_coefs=None, clear_session=True):
     if sigma_coefs is None:
         sigma_coefs = [-2.57583, -1.95996, -0.974114, -0.674, 0, 0.674, 0.9741114, 1.95996, 2.57583]
 
@@ -60,8 +60,8 @@ def get_simple_dist_model(inp_shape, num_nodes=64, sigma_coefs=None, clear_sessi
     x = Dense(num_nodes, activation="relu")(x)
     x = Dense(num_nodes, activation="relu")(x)
 
-    mu = Dense(1)(x)  # represents mu
-    sigma = Dense(1, activation="relu")(x)  # represents sigma
+    mu = Dense(1, activation=final_activation)(x)  # represents mu
+    sigma = Dense(1, activation=final_activation)(x)  # represents sigma
 
     outs = []
 
@@ -75,7 +75,8 @@ def get_simple_dist_model(inp_shape, num_nodes=64, sigma_coefs=None, clear_sessi
     return model
 
 
-def get_simple_dense_model(inp_shape, num_nodes=64, num_layers=3, bottleneck_nodes=2, clear_session=True):
+def get_simple_dense_model(inp_shape, num_nodes=64, num_layers=3, bottleneck_nodes=None,
+                           final_activation="linear", clear_session=True):
     if clear_session:
         # clear previous sessions
         K.clear_session()
@@ -87,17 +88,18 @@ def get_simple_dense_model(inp_shape, num_nodes=64, num_layers=3, bottleneck_nod
     for i in range(num_layers):
         x = Dense(num_nodes, activation="relu")(x)
 
-    x = Dense(bottleneck_nodes)(x)  # represents mu, sigma
+    if bottleneck_nodes is not None:
+        x = Dense(bottleneck_nodes, activation="relu")(x)  # represents mu, sigma
 
-    out_q0 = Dense(1, name="q0")(x)
-    out_q1 = Dense(1, name="q1")(x)
-    out_q2 = Dense(1, name="q2")(x)
-    out_q3 = Dense(1, name="q3")(x)
-    out_q4 = Dense(1, name="q4")(x)
-    out_q5 = Dense(1, name="q5")(x)
-    out_q6 = Dense(1, name="q6")(x)
-    out_q7 = Dense(1, name="q7")(x)
-    out_q8 = Dense(1, name="q8")(x)
+    out_q0 = Dense(1, name="q0", activation=final_activation)(x)
+    out_q1 = Dense(1, name="q1", activation=final_activation)(x)
+    out_q2 = Dense(1, name="q2", activation=final_activation)(x)
+    out_q3 = Dense(1, name="q3", activation=final_activation)(x)
+    out_q4 = Dense(1, name="q4", activation=final_activation)(x)
+    out_q5 = Dense(1, name="q5", activation=final_activation)(x)
+    out_q6 = Dense(1, name="q6", activation=final_activation)(x)
+    out_q7 = Dense(1, name="q7", activation=final_activation)(x)
+    out_q8 = Dense(1, name="q8", activation=final_activation)(x)
 
     model = Model(inputs=inp, outputs=[out_q0, out_q1, out_q2, out_q3, out_q4, out_q5, out_q6, out_q7, out_q8])
 
@@ -184,8 +186,8 @@ def get_extended_dist_model(inp_shape, sigma_coefs, clear_session=True):
     return model
 
 
-def get_variable_dist_model(inp_shape, sigma_coefs, num_nodes=64, clear_sesion=True):
-    if clear_sesion:
+def get_variable_dist_model(inp_shape, sigma_coefs, num_nodes=64, final_activation="exponential", clear_session=True):
+    if clear_session:
         # clear previous sessions
         K.clear_session()
 
@@ -195,8 +197,8 @@ def get_variable_dist_model(inp_shape, sigma_coefs, num_nodes=64, clear_sesion=T
     x = Dense(num_nodes, activation='relu')(x)
     x = Dense(num_nodes, activation='relu')(x)
 
-    mu = Dense(1, activation="exponential")(x)  # represents mu
-    sigma = Dense(1, activation="exponential")(x)  # represents sigma
+    mu = Dense(1, activation=final_activation)(x)  # represents mu
+    sigma = Dense(1, activation=final_activation)(x)  # represents sigma
     kurtosis = Dense(1)(x)  # represents kurtosis
     skewness = Dense(1)(x)  # represents skewness
     outs = []
@@ -249,7 +251,9 @@ def get_direct_custom_layer(i):
     return custom_layer
 
 
-def get_direct_dist_model(inp_shape, num_nodes=256, clear_session=True):
+def get_direct_dist_model(inp_shape, num_nodes=256, final_activation=None, clear_session=True):
+    # final activation parameter only for compatibility
+
     if clear_session:
         # clear previous sessions
         K.clear_session()
